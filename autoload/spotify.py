86f7cd6c7cf3f31b2682ee28780ff4428141b791
Christopher Chapline <cachapline8@gmail.com>
2015-07-30 08:12:39 -0600

    WIP: Refactoring and adding albums/artists

--- autoload/spotify.py
+++ autoload/spotify.py
@@ -6,51 +6,71 @@ import pprint
 API_URL = "https://api.spotify.com/v1/search"
 TIMEOUT = 10
 
-def search_track():
-    # Get the user's input
+def _user_input(message, variable_name):
+    input_command = "let {variable_name} = inputdialog('{message}')"
+
     vim.command("call inputsave()")
-    vim.command("let user_input = inputdialog('Enter your query: ')")
+    vim.command(input_command.format(message = message, variable_name = variable_name))
     vim.command("call inputrestore()")
     vim.command("echo '\n'")
-    track_name = vim.eval("user_input")
+    return vim.eval(variable_name)
+
+def search_spotify(type="track", limit=20):
+    # Get the user's input
+    track_name = _user_input("Enter the {type} name: ".format(type=type), "track_name")
 
     # Send the request
-    request_url = "%s?query=%s&type=track&limit=50" % (API_URL, track_name)
+    request_url = "{url}?query={query}&type={type}&limit={limit}"
+    request_url = request_url.format(url=API_URL, query=track_name, type=type, limit=limit)
+
     try:
         response_content = requests.get(request_url, timeout = TIMEOUT).json()
     except requests.exceptions.Timeout as Timeout:
         print "Your query timed out!"
         return
-    items = response_content["tracks"]["items"]
 
-    items_list = {}
-    index = 1
-    for item in items:
-        print "%d: %s - %s" % (index, item["name"], item["artists"][0]["name"])
-        items_list[index] = item
-        index += 1
+    item_key = "{type}s".format(type=type)
+    items = response_content[item_key]["items"]
 
-    # Get the selected song
-    vim.command("call inputsave()")
-    vim.command("let user_input = inputdialog('Select a track number: ')")
-    vim.command("call inputrestore()")
-    vim.command("echo '\n'")
+    # Print the found results
+    if type == "track":
+        _parse_tracks(items)
+    elif type == "artist":
+        _parse_artists(items)
+    elif type == "album":
+        _parse_albums(items)
+
+    # Get the selected item
+    item_num = _user_input("Select a {type} number: ".format(type = type), "item_num")
 
     try:
-        track_num = int(vim.eval("user_input"))
+        item_num = int(item_num)
     except:
         print "%s is not a valid selection!" % vim.eval("user_input")
         return
 
     # Play the track
-    selected_item = items_list[track_num]
-    print "Playing %s by %s..." % (selected_item["name"], selected_item["artists"][0]["name"])
-    play_spotify_track(selected_item["uri"])
+    selected_item = items[item_num]
+    print "Playing %s" % selected_item["name"]
+    play_uri(selected_item["uri"])
+
+def _parse_tracks(tracks):
+    for track_number, track in enumerate(tracks):
+        print "%d: %s - %s" % (track_number, track["name"], track["artists"][0]["name"])
+
+def _parse_artists(artists):
+    for artist_number, artist in enumerate(artists):
+        print "%d: %s" % (artist_number, artist["name"])
+
+def _parse_albums(albums):
+    for album_number, album in enumerate(albums):
+        print "%d: %s" % (album_number, album["name"])
 
 def shell_command(*args):
     return subprocess.check_output(list(args))
 
 def spotify_command(command, *args):
+    print "dbus-send --dest=org.mpris.MediaPlayer2.spotify --print-reply /org/mpris/MediaPlayer2 %s %s" % (command, " ".join(args))
     x = shell_command("dbus-send",
             "--dest=org.mpris.MediaPlayer2.spotify",
             "--print-reply",
@@ -70,6 +90,6 @@ def previous_song():
     """Moves to the previous track"""
     spotify_command("org.mpris.MediaPlayer2.Player.Previous")
 
-def play_spotify_track(song_uri):
+def play_uri(uri):
     """Plays the song that with the provided URI"""
-    spotify_command("org.mpris.MediaPlayer2.Player.OpenUri", "string:%s" % song_uri)
+    spotify_command("org.mpris.MediaPlayer2.Player.OpenUri", "string:%s" % uri)
--- autoload/spotify.vim
+++ autoload/spotify.vim
@@ -3,7 +3,8 @@ if !has("python")
     finish
 end
 
-pyfile ./autoload/spotify.py
+let s:plugin_path = escape(expand('<sfile>:p:h'), '\')
+exe 'pyfile ' . s:plugin_path . '/spotify.py'
 
 function! spotify#next_song()
     python next_song()
@@ -18,11 +19,21 @@ function! spotify#pause_unpause()
 endfunction
 
 function! spotify#search_track()
-    python search_track()
+    python search_spotify(type = "track")
+endfunction
+
+function! spotify#search_album()
+    python search_spotify(type = "album")
+endfunction
+
+function! spotify#search_artist()
+    python search_spotify(type = "artist")
 endfunction
 
 " Bindings
 command! FindTrack call spotify#search_track()
+command! FindAlbum call spotify#search_album()
+command! FindArtist call spotify#search_artist()
 command! NextSong call spotify#next_song()
 command! PreviousSong call spotify#previous_song()
 command! PauseUnpause call spotify#pause_unpause()
