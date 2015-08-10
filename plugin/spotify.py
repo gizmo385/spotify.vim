1c5a5104c9ed081b339de89e6665f0f0f24a98e8
Christopher Chapline <cachapline8@gmail.com>
2015-08-10 00:44:24 -0600

    Working on implementing adapter pattern
    Creating a separate adapter for osascript and dbus-send will help clean up the code for sending and
    parsing the commands for interacting with spotify. Right now, the OsascriptAdapter is working and
    the DbusAdapter is stubbed out.

--- plugin/spotify.py
+++ plugin/spotify.py
@@ -1,6 +1,8 @@
+from __future__ import unicode_literals
 import requests
 import vim
-import os
+import subprocess
+import json
 
 from sys import platform, exit
 
@@ -8,13 +10,8 @@ from sys import platform, exit
 SEARCH_API_URL = "https://api.spotify.com/v1/search"
 TIMEOUT = 10
 
-# Command keys
-PLAY_URI = "playUri"
-NEXT_SONG = "nextSong"
-PREVIOUS_SONG = "previousSong"
-PLAY_PAUSE = "playPause"
-
-commands = {k: None for k in [PLAY_URI, NEXT_SONG, PREVIOUS_SONG, PLAY_PAUSE]}
+# Handles communication using the proper mechanism for the operating system
+adapter = None
 
 
 def _user_input(message, variable_name):
@@ -45,7 +42,9 @@ def search_spotify(type="track"):
                                      type=type, limit=limit)
 
     try:
-        response_content = requests.get(request_url, timeout=TIMEOUT).json()
+        response_content = requests.get(request_url, timeout=TIMEOUT).text
+        response_content = response_content.encode("ascii", "ignore")
+        response_content = json.loads(response_content)
     except requests.exceptions.Timeout as Timeout:
         print "Your query timed out!"
         return
@@ -76,9 +75,8 @@ def search_spotify(type="track"):
 
     # Play the track
     selected_item = items[item_num]
-    vim.command("redraw!")
-    print "Playing %s" % selected_item["name"]
-    play_uri('"{item}"'.format(item=selected_item["uri"]))
+    # vim.command("redraw!")
+    adapter.play_uri('"{item}"'.format(item=selected_item["uri"]))
 
 
 def _parse_tracks(tracks):
@@ -86,36 +84,33 @@ def _parse_tracks(tracks):
         status = "{number}: {name} - {artist}"
         artist_name = track["artists"][0]["name"]
 
-        print status.format(number=track_number, name=track["name"],
-                            artist=artist_name)
+        message = status.format(number=track_number, name=track["name"],
+                                artist=artist_name)
+
+        print message
 
 
 def _parse_artists(artists):
     for artist_number, artist in enumerate(artists):
+        status = "{number}: {artist}"
         print "%d: %s" % (artist_number, artist["name"])
 
 
 def _parse_albums(albums):
     for album_number, album in enumerate(albums):
+        status = "{number}: {album}"
         print "%d: %s" % (album_number, album["name"])
 
 
 def shell_command(*args):
-    return os.system(" ".join(args))
+    command = shlex.split(" ".join(args).strip())
+    p = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+    response = p.communicate()[0].strip()
+    return response.encode("ascii", "ignore").strip()
 
 
-def osascript_command(command, *args):
-    command_string = "'tell application \"Spotify\" to {command}"
-    command_string = command_string.format(command=command)
-
-    if args:
-        command_string += " {args}".format(args="  ".join(args))
-    else:
-        command_string += "'"
-
-    shell_command("osascript", "-e", command_string)
-
-
+# This will be removed
 def dbus_command(command, *args):
     args = list(args)
     args.append(">/dev/null")
@@ -124,51 +119,96 @@ def dbus_command(command, *args):
                       *args)
 
 
-def pause_unpause():
-    """Toggles between paused and unpaused"""
-    spotify_command(commands[PLAY_PAUSE])
+class SpotifyAdapter(object):
+
+    def play_track(self, track_name):
+        error = "Must be implemented by subclass of {cls}"
+        raise NotImplementedError(error.format(cls=self.__class__.__name__))
+
+    def play_album(self, album_name):
+        error = "Must be implemented by subclass of {cls}"
+        raise NotImplementedError(error.format(cls=self.__class__.__name__))
+
+    def play_artist(self, artist_name):
+        error = "Must be implemented by subclass of {cls}"
+        raise NotImplementedError(error.format(cls=self.__class__.__name__))
+
+    def print_current_track(self):
+        error = "Must be implemented by subclass of {cls}"
+        raise NotImplementedError(error.format(cls=self.__class__.__name__))
 
 
-def next_song():
-    """Skips to the next track"""
-    spotify_command(commands[NEXT_SONG])
+class OsascriptAdapter(SpotifyAdapter):
+    # Commands to send
+    PLAY_URI = "play track"
+    NEXT_SONG = "next track"
+    PREVIOUS_SONG = "previous track"
+    PLAY_PAUSE = "playpause"
+    CURRENT_SONG = "name of current track"
+    CURRENT_ARTIST = "artist of current track"
+    CURRENT_ALBUM = "album of current track"
+
+    def __init__(self):
+        super(OsascriptAdapter, self).__init__()
+
+    def _send_command(self, command, *args):
+        command_string = "'tell application \"Spotify\" to {command}"
+        command_string = command_string.format(command=command)
+
+        if args:
+            command_string += " {args}".format(args="  ".join(args))
+        else:
+            command_string += "'"
+
+        return shell_command("osascript", "-e", command_string)
+
+    def print_current_track(self):
+        status = "Playing {song} by {artist} on {album}"
+
+        song = self._send_command(OsascriptAdapter.CURRENT_SONG)
+        artist = self._send_command(OsascriptAdapter.CURRENT_ARTIST)
+        album = self._send_command(OsascriptAdapter.CURRENT_ALBUM)
+        vim.command("redraw!")
+
+        print status.format(song=song, artist=artist, album=album)
+
+    def play_uri(self, uri):
+        command = "{command} {uri}"
+        self._send_command(command.format(command=OsascriptAdapter.PLAY_URI,
+                                          uri=uri))
+        self.print_current_track()
+
+    def next_track(self):
+        self._send_command(OsascriptAdapter.NEXT_SONG)
+        self.print_current_track()
+
+    def previous_track(self):
+        self._send_command(OsascriptAdapter.PREVIOUS_SONG)
+        self.print_current_track()
+
+    def pause_unpause(self):
+        self._send_command(OsascriptAdapter.PLAY_PAUSE)
 
 
-def previous_song():
-    """Moves to the previous track"""
-    spotify_command(commands[PREVIOUS_SONG])
+class DbusAdapter(SpotifyAdapter):
+    # Commands to send
+    PLAY_URI = "org.mpris.MediaPlayer2.Player.OpenUri"
+    NEXT_SONG = "org.mpris.MediaPlayer2.Player.Next"
+    PREVIOUS_SONG = "org.mpris.MediaPlayer2.Player.Previous"
+    PLAY_PAUSE = "org.mpris.MediaPlayer2.Player.PlayPause"
 
+    def __init__(self):
+        super(DbusAdapter, self).__init__()
 
-def play_uri(uri):
-    """Plays the song that with the provided URI"""
-    if platform == "darwin":
-        spotify_command(commands[PLAY_URI], uri)
-    elif platform in ["linux", "linux2"]:
-        spotify_command(commands[PLAY_URI], "string:%s" % uri)
-    else:
-        print "Your platform is invalid!"
-        exit(1)
+    def current_song():
+        pass
 
 
 # Set the commands based on what platform this is running on
 if platform == "win32":  # Windows
     print "This plugin requires OSX or Linux!"
     exit(1)
-
 elif platform == "darwin":  # OSX
-    commands = {
-        PLAY_URI: "play track",
-        NEXT_SONG: "next track",
-        PREVIOUS_SONG: "previous track",
-        PLAY_PAUSE: "playpause"
-    }
-    spotify_command = osascript_command
-
+    adapter = OsascriptAdapter()
 elif platform in ["linux", "linux2"]:  # Linux
-    commands = {
-        PLAY_URI: "org.mpris.MediaPlayer2.Player.OpenUri",
-        NEXT_SONG: "org.mpris.MediaPlayer2.Player.Next",
-        PREVIOUS_SONG: "org.mpris.MediaPlayer2.Player.Previous",
-        PLAY_PAUSE: "org.mpris.MediaPlayer2.Player.PlayPause"
-    }
-    spotify_command = dbus_command
+    adapter = DbusAdapter()
--- plugin/spotify.vim
+++ plugin/spotify.vim
@@ -12,9 +12,10 @@ exe 'pyfile ' . s:plugin_path . '/spotify.py'
 let g:spotify_limit = 20
 
 " Bindings
-command! Track              exec 'python search_spotify(type = "track")'
-command! Album              exec 'python search_spotify(type = "album")'
-command! Artist             exec 'python search_spotify(type = "artist")'
-command! NextSong           exec 'python next_song()'
-command! PreviousSong       exec 'python previous_song()'
-command! PauseUnpause       exec 'python pause_unpause()'
+command! Track              exec 'python search_spotify(type="track")'
+command! Album              exec 'python search_spotify(type="album")'
+command! Artist             exec 'python search_spotify(type="artist")'
+command! NextSong           exec 'python adapter.next_track()'
+command! PreviousSong       exec 'python adapter.previous_track()'
+command! PauseUnpause       exec 'python adapter.pause_unpause()'
+command! CurrentTrack       exec 'python adapter.print_current_track()'
